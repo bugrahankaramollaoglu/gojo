77c30c26923af57927788a692c9297bcbe100329
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-08-07 09:51:21 +0300

    will try another bottomNav

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,8 @@
+import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gojo/deneme.dart';
-import 'package:gojo/login_page.dart';
+import 'package:gojo/main.dart';
+import 'package:iconly/iconly.dart';
 
 Future<void> signOut() async {
   try {
@@ -20,24 +21,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0; // Track the selected index
-  Widget currentBody = Container(
-    child: Center(
-      child: Text(
-        'Home Page',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 30,
-        ),
-      ),
-    ),
-  );
+  int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      if (index == 0) {
-        currentBody = Container(
+  Widget _getBody(int selectedIndex) {
+    switch (selectedIndex) {
+      case 0:
+        return Container(
           child: Center(
             child: Text(
               'Home Page',
@@ -48,15 +37,50 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         );
-      } else if (index == 1) {
-        setState(() {
-          currentBody = Page1();
-        });
-      } else if (index == 2) {
-        setState(() {
-          currentBody = Page2();
-        });
-      }
+      case 1:
+        return Container(
+          child: Center(
+            child: Text(
+              'Search Page',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        );
+      case 2:
+        return Container(
+          child: Center(
+            child: Text(
+              'Add Page',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        );
+      case 3:
+        return Container(
+          child: Center(
+            child: Text(
+              'Profile Page',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        );
+      default:
+        return Container();
+    }
+  }
+
+  void _handleIndexChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -64,10 +88,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 25, 25, 25),
-      body: currentBody,
-      bottomNavigationBar: BottomNav(
-        selectedIndex: _selectedIndex, // Pass the selected index
-        onItemTapped: _onItemTapped, // Pass the function to handle taps
+      body: _getBody(_selectedIndex),
+      bottomNavigationBar: CrystalNavigationBar(
+        duration: const Duration(milliseconds: 500),
+        currentIndex: _selectedIndex,
+        height: 50,
+        borderRadius: 15,
+        splashBorderRadius: 15,
+        indicatorColor: Colors.blue,
+        backgroundColor: Colors.transparent,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _handleIndexChanged,
+        enablePaddingAnimation: false,
+        items: [
+          CrystalNavigationBarItem(
+            icon: IconlyBold.home,
+            unselectedIcon: IconlyLight.home,
+          ),
+          CrystalNavigationBarItem(
+            icon: IconlyBold.search,
+            unselectedIcon: IconlyLight.search,
+          ),
+          CrystalNavigationBarItem(
+            icon: IconlyBold.plus,
+            unselectedIcon: IconlyLight.plus,
+          ),
+          CrystalNavigationBarItem(
+            icon: IconlyBold.profile,
+            unselectedIcon: IconlyLight.profile,
+          ),
+        ],
       ),
     );
   }
