666863223ef2c49a80419d001587f460d5ee033a
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-08-08 11:30:09 +0300

    will try recording

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -37,7 +37,7 @@ class AddPage extends ConsumerWidget {
   DateTime? _selectedDateTime;
 
   // Function to show the date and time picker dialog
-  Future<void> _selectDateTime(BuildContext context) async {
+  Future<void> selectDateTime(BuildContext context) async {
     // Show date picker
     DateTime? pickedDate = await showDatePicker(
       context: context,
@@ -114,228 +114,287 @@ class AddPage extends ConsumerWidget {
             ),
           ),
           Positioned(
-              top: 80,
-              right: 25,
-              child: Row(
-                children: [
-                  ShimmerEffect(
-                    baseColor: Colors.white,
-                    highlightColor: Colors.white54,
-                    loop: 1,
-                    child: GestureDetector(
-                      onTap: () {
-                        if (ref.read(recordedNoteChosen.notifier).state) {
-                          ref.read(recordedNoteChosen.notifier).state =
-                              !ref.read(recordedNoteChosen);
-                        }
-                      },
-                      child: Column(
-                        children: [
-                          Text(
-                            'Write',
-                            style: GoogleFonts.kreon(
-                              color: isRecordedChosen
-                                  ? Colors.white70
-                                  : Colors.white,
-                              fontSize: 25,
-                              fontWeight: isRecordedChosen
-                                  ? FontWeight.w100
-                                  : FontWeight.bold,
-                            ),
-                          ),
-                          Icon(
-                            IconlyBold.paper,
-                            color: isRecordedChosen
-                                ? Colors.white70
-                                : Colors.white,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      'or',
-                      style: GoogleFonts.kreon(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
+            top: 80,
+            right: 25,
+            child: Row(
+              children: [
+                ShimmerEffect(
+                  baseColor: Colors.white,
+                  highlightColor: Colors.white54,
+                  loop: 1,
+                  child: GestureDetector(
                     onTap: () {
-                      if (!ref.read(recordedNoteChosen)) {
+                      if (ref.read(recordedNoteChosen.notifier).state) {
                         ref.read(recordedNoteChosen.notifier).state =
                             !ref.read(recordedNoteChosen);
                       }
                     },
-                    child: ShimmerEffect(
-                      baseColor: Colors.white,
-                      highlightColor: Colors.white54,
-                      loop: 1,
-                      child: Column(
-                        children: [
-                          Text(
-                            'Record',
-                            style: GoogleFonts.kreon(
-                              color: isRecordedChosen
-                                  ? Colors.white
-                                  : Colors.white70,
-                              fontSize: 25,
-                              fontWeight: isRecordedChosen
-                                  ? FontWeight.bold
-                                  : FontWeight.w100,
-                            ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Write',
+                          style: GoogleFonts.kreon(
+                            color: isRecordedChosen
+                                ? Colors.white70
+                                : Colors.white,
+                            fontSize: 25,
+                            fontWeight: isRecordedChosen
+                                ? FontWeight.w100
+                                : FontWeight.bold,
                           ),
-                          Icon(
-                            IconlyBold.voice_2,
+                        ),
+                        Icon(
+                          IconlyBold.paper,
+                          color:
+                              isRecordedChosen ? Colors.white70 : Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    'or',
+                    style: GoogleFonts.kreon(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (!ref.read(recordedNoteChosen)) {
+                      ref.read(recordedNoteChosen.notifier).state =
+                          !ref.read(recordedNoteChosen);
+                    }
+                  },
+                  child: ShimmerEffect(
+                    baseColor: Colors.white,
+                    highlightColor: Colors.white54,
+                    loop: 1,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Record',
+                          style: GoogleFonts.kreon(
                             color: isRecordedChosen
                                 ? Colors.white
                                 : Colors.white70,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 150),
-                Text(
-                  'Title of Note (*)',
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white70,
-                      ),
-                ),
-                SizedBox(height: 10),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  elevation: 5,
-                  child: TextField(
-                    controller: _titleController,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(16),
-                      hintText: 'Enter the title of your note...',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Deadline & Category',
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white70,
-                      ),
-                ),
-                SizedBox(height: 10),
-                // Fixed Row Layout
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2, // 2 parts of the width
-                      child: GestureDetector(
-                        onTap: () => _selectDateTime(context),
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Icon(
-                              Icons.calendar_month_rounded,
-                              size: 50,
-                            ),
+                            fontSize: 25,
+                            fontWeight: isRecordedChosen
+                                ? FontWeight.bold
+                                : FontWeight.w100,
                           ),
                         ),
-                      ),
-                    ),
-                    SizedBox(width: 16), // Add spacing between cards
-                    Expanded(
-                      flex: 4, // 4 parts of the width
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        Icon(
+                          IconlyBold.voice_2,
+                          color:
+                              isRecordedChosen ? Colors.white : Colors.white70,
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Icon(
-                            Icons.category_rounded,
-                            size: 45,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Note Description',
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white70,
-                      ),
-                ),
-                SizedBox(height: 10),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  elevation: 5,
-                  child: TextField(
-                    controller: _descriptionController,
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(16),
-                      hintText: 'Enter the description of your note...',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // signOut(context);
-                      // _saveNote();
-                      print('isRecordedChosen: $isRecordedChosen');
-                    },
-                    child: ShimmerEffect(
-                      baseColor: Colors.white,
-                      highlightColor: Colors.blue,
-                      loop: 5,
-                      child: Text(
-                        'Save Note',
-                        style: GoogleFonts.kreon(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                      textStyle: TextStyle(fontSize: 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                      ],
                     ),
                   ),
                 ),
               ],
             ),
           ),
+          Positioned(
+              child: isRecordedChosen
+                  ? recordedNoteContent()
+                  : writtenNoteContent(context, _titleController,
+                      _descriptionController, isRecordedChosen)),
         ],
       ),
     );
   }
+}
+
+Widget recordedNoteContent() {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 150),
+        Text(
+          'Recorded Note',
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 20),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // signOut(context);
+              // _saveNote();
+            },
+            child: ShimmerEffect(
+              baseColor: Colors.white,
+              highlightColor: Colors.blue,
+              loop: 5,
+              child: Text(
+                'Save Note',
+                style: GoogleFonts.kreon(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              textStyle: TextStyle(fontSize: 18),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget writtenNoteContent(
+    BuildContext context,
+    TextEditingController _titleController,
+    TextEditingController _descriptionController,
+    bool isRecordedChosen) {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 150),
+        Text(
+          'Title of Note (*)',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
+        ),
+        SizedBox(height: 10),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 5,
+          child: TextField(
+            controller: _titleController,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(16),
+              hintText: 'Enter the title of your note...',
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Deadline & Category',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
+        ),
+        SizedBox(height: 10),
+        // Fixed Row Layout
+        Row(
+          children: [
+            Expanded(
+              flex: 2, // 2 parts of the width
+              child: GestureDetector(
+                // onTap: () => selectDateTime(context),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Icon(
+                      Icons.calendar_month_rounded,
+                      size: 50,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 16), // Add spacing between cards
+            Expanded(
+              flex: 4, // 4 parts of the width
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Icon(
+                    Icons.category_rounded,
+                    size: 45,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Note Description',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
+        ),
+        SizedBox(height: 10),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 5,
+          child: TextField(
+            controller: _descriptionController,
+            maxLines: 5,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(16),
+              hintText: 'Enter the description of your note...',
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        SizedBox(height: 30),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // signOut(context);
+              // _saveNote();
+              print('isRecordedChosen: $isRecordedChosen');
+            },
+            child: ShimmerEffect(
+              baseColor: Colors.white,
+              highlightColor: Colors.blue,
+              loop: 5,
+              child: Text(
+                'Save Note',
+                style: GoogleFonts.kreon(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              textStyle: TextStyle(fontSize: 18),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
